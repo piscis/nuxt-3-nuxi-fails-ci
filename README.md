0f305c937f137a2f44c054a38e1595398d5fb202
piscis <self@pirsig.net>
2023-02-11 13:43:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-3-nuxi-fails-ci
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt-starter-el5s4f)
---
+++ app.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const date = useLocaleDate(new Date(`2022-01-01`));
+</script>
+
+<template>
+  <div>{{ date }}</div>
+</template>
